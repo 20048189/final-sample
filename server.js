2ee3584e027aaaa4eb587d13a6bcc59f8e2e9cc3
umair <20048189@mydbs.ie>
2024-12-11 05:03:49 +0000

    new

--- server.js
+++ server.js
@@ -1,58 +1,34 @@
 const express = require('express');
 const fs = require('fs');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
-
-// Middleware
-app.use(cors());
-app.use(bodyParser.json());
-
-// File path for storing questions
 const DATA_FILE = './questions.json';
 
-// Utility function to read/write JSON file
-const readQuestions = () => JSON.parse(fs.readFileSync(DATA_FILE, 'utf-8'));
-const writeQuestions = (data) => fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
+app.use(cors());
+app.use(express.json());
 
-// API Endpoints
+// Root Route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Quiz API');
+});
 
 // Get all questions
 app.get('/api/questions', (req, res) => {
-  const questions = readQuestions();
+  const questions = JSON.parse(fs.readFileSync(DATA_FILE, 'utf-8'));
   res.json(questions);
 });
 
 // Add a question
 app.post('/api/questions', (req, res) => {
-  const questions = readQuestions();
-  const newQuestion = req.body;
-  questions.push(newQuestion);
-  writeQuestions(questions);
-  res.status(201).json(newQuestion);
+  const questions = JSON.parse(fs.readFileSync(DATA_FILE, 'utf-8'));
+  questions.push(req.body);
+  fs.writeFileSync(DATA_FILE, JSON.stringify(questions, null, 2));
+  res.status(201).json(req.body);
 });
 
-// Update a question
-app.put('/api/questions/:index', (req, res) => {
-  const questions = readQuestions();
-  const { index } = req.params;
-  questions[index] = req.body;
-  writeQuestions(questions);
-  res.json(questions[index]);
-});
-
-// Delete a question
-app.delete('/api/questions/:index', (req, res) => {
-  const questions = readQuestions();
-  const { index } = req.params;
-  const deletedQuestion = questions.splice(index, 1);
-  writeQuestions(questions);
-  res.json(deletedQuestion);
-});
-
-// Start server
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on https://stunning-tribble-wrgvg6vx69r525579-3000.app.github.dev/`);
 });
