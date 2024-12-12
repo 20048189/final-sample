a70f8a1b6a4c9ee1803e3d2b100a5a11da3b8acc
umair <20048189@mydbs.ie>
2024-12-12 14:36:28 +0000

    new

--- server.js
+++ server.js
@@ -23,11 +23,17 @@ app.get('/api/questions', (req, res) => {
 
 // Add a new question
 app.post('/api/questions', (req, res) => {
-  const questions = JSON.parse(fs.readFileSync(DATA_FILE, 'utf-8') || '[]');
-  questions.push(req.body);
-  fs.writeFileSync(DATA_FILE, JSON.stringify(questions, null, 2));
-  res.status(201).json(req.body);
+  try {
+    const questions = JSON.parse(fs.readFileSync(DATA_FILE, 'utf-8') || '[]');
+    questions.push(req.body);
+    fs.writeFileSync(DATA_FILE, JSON.stringify(questions, null, 2));
+    res.status(201).json(req.body);
+  } catch (error) {
+    console.error('Error writing to file:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
+
 
 // Update a question
 app.put('/api/questions/:index', (req, res) => {
