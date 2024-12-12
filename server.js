97605fb1da76127e9eb3cdf45071d1f14acca4de
umair <20048189@mydbs.ie>
2024-12-12 13:51:52 +0000

    new

--- server.js
+++ server.js
@@ -10,10 +10,13 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/', {
+mongoose.connect('mongodb://127.0.0.1:27017/quiz', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
